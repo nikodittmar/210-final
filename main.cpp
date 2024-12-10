2c0705836502af270952d63bc7cf072b2ba4e127
Niko Dittmar <nikodittmar@gmail.com>
2024-12-09 18:45:19 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,48 @@
 
 using namespace std;
 
+struct CoffeeBooth {
+    CustomerNode* queue;
+
+public:
+    void addCustomer() {
+        
+    }
+};
+
+struct CustomerNode {
+    string name;
+    string order;
+    CustomerNode* next;
+};
+
 int main() {
+    string names[10] = {
+        "Bob",
+        "Joe",
+        "Sean",
+        "David",
+        "Adam",
+        "Luke",
+        "Henry",
+        "Javier",
+        "Mike",
+        "Charles"
+    };
+
+    string orders[10] = {
+        "Frappuccino",
+        "Espresso",
+        "Iced Coffee",
+        "Americano",
+        "Latte",
+        "Cold Brew",
+        "Macchiato",
+        "Mocha",
+        "Mike",
+        "Cappuccino"
+    };
+
+
     cout << "Hello World!";
 }
