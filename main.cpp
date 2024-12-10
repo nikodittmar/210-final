0a724bcab431e14c9a8cd5e2dadecf051a3c2712
Niko Dittmar <nikodittmar@gmail.com>
2024-12-09 18:55:10 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -2,19 +2,44 @@
 
 using namespace std;
 
-struct CoffeeBooth {
-    CustomerNode* queue;
-
-public:
-    void addCustomer() {
-        
-    }
-};
-
 struct CustomerNode {
     string name;
     string order;
     CustomerNode* next;
+public:
+    CustomerNode(string n, string o) {
+        name = n;
+        order = o;
+        next = nullptr;
+    }
+};
+
+struct CoffeeBooth {
+    CustomerNode* queue;
+
+public:
+    void addCustomer(string name, string order) {
+        CustomerNode *current = queue;
+        while (queue) {
+            current = queue->next;
+        }
+
+        CustomerNode customer(name, order);
+
+        if (queue) {
+            queue->next = &customer;
+        } else {
+            queue = &customer;
+        }
+    }
+
+    void serveCustomer() {
+        queue = queue->next;
+    }
+
+    void printQueue() {
+
+    }
 };
 
 int main() {
